7e712b68bc53caccd9c17dd4ece4bc6f7bd43100
Saleem Rashid <spudowiar@outlook.com>
2015-07-27 20:45:49 +0100

    routes: 404 handler

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,26 @@
 var router = require("express").Router();
 module.exports = router;
+
+router.use(function (request, response, next) {
+    response.status(404);
+    response.format({
+        /* plain text error */
+        "text/plain": function () {
+            response.send("404 Not Found");
+        },
+
+        /* valid JSON */
+        "application/json": function () {
+            response.send({ error: 404 });
+        },
+
+        /* fancy error page */
+        "text/html": function () {
+            response.render("error");
+        },
+
+        "default": function () {
+            res.status(406); /* 406 Not Acceptable */
+        }
+    });
+});
