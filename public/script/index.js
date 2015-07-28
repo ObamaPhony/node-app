c42194a05c7340e7b9051be58f91cc5d511046c4
Saleem Rashid <spudowiar@outlook.com>
2015-07-28 16:14:39 +0100

    index: YAY! WORKING ENTER STUFF!

---
+++ public/script/index.js
@@ -0,0 +1,19 @@
+$(function () {
+    var punctuation = /[^a-zA-Z0-9 ]+/;
+    function fix(value) {
+        return value.replace(punctuation, "").replace(/\s/, " ").replace(/ +/, " ");
+    }
+
+    $("#userInput").keydown(function (e) {
+        $(this).val(fix($(this).val()));
+
+        /* enter key */
+        if (e.keyCode == 13) {
+            $("<input type='text' /><br />").val($(this).val()).appendTo("#nouns-content").keydown(function (e) {
+                $(this).val(fix($(this).val()));
+            });
+            $(this).val("");
+            $("#nouns").animate({ scrollTop: $("#nouns")[0].scrollHeight });
+        }
+    });
+});
