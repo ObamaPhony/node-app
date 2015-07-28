b857101ac3881c0851e6edbaf3f5d12eae33f597
Saleem Rashid <spudowiar@outlook.com>
2015-07-28 16:56:08 +0100

    index: begin Google trends

--- public/script/index.js
+++ public/script/index.js
@@ -1,4 +1,17 @@
 $(function () {
+    $.ajax({
+        url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://www.google.com/trends/hottrends/atom/feed?pn=p1",
+        jsonp: "callback",
+        dataType: "jsonp",
+        success: function (data) {
+            var trends = [];
+            var feed = data.responseData.feed.entries;
+            for (var i = 0; i < feed.length; i++) {
+                trends.push(feed[i].title);
+            }
+        }
+    });
+
     var punctuation = /[^a-zA-Z0-9 ]+/;
     function fix(value) {
         return value.replace(punctuation, "").replace(/\s/, " ").replace(/ +/, " ");
