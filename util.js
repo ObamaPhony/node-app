2dc9a2fb5aac8d74a333bf25e99f879c589a9ea5
Saleem Rashid <spudowiar@outlook.com>
2015-07-30 08:51:01 +0100

    app: logging

--- util.js
+++ util.js
@@ -1,10 +1,43 @@
 var spawn = require("child_process").spawn;
+var winston = require("winston");
+
+exports.logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: './app.log',
+            handleExceptions: true,
+            maxSize: 5242880,
+            json: true,
+            maxFiles: 5,
+            colorize: false
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+    exitOnError: false
+});
+
+exports.log = {
+    write: function (message, encoding) {
+        exports.logger.info(message)
+    }
+};
 
 exports.status = function (response, code) {
     response.status(code);
     response.json({
         err: code
     });
+};
+
+exports.error = function (response, err) {
+    this.status(response, 500);
+    this.logger.error(err);
 };
 
 exports.spawn = function (bin, stdin, next) {
