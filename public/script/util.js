a8e74be3a94e326d3938b7c5b63ee8d6922b7d23
Saleem Rashid <dev@saleemrashid.com>
2015-07-31 18:38:09 +0100

    util: I'm sad enough to support IE7

--- public/script/util.js
+++ public/script/util.js
@@ -1,10 +1,18 @@
+String.prototype.trim = String.prototype.trim || function () {
+    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
+};
+
+Array.isArray = Array.isArray || function (arg) {
+    return Object.prototype.toString.call(arg) == "[object Array]";
+}
+
 function API(name, args, callback) {
     if (typeof args === "function") {
         callback = args;
         args = [];
     }
 
-    if (Array.isArray(args)) {
+    if (Array.isArray && Array.isArray(args)) {
         $.getJSON("/api/" + name + "/" + args.join("/"), callback);
     } else {
         $.ajax({
